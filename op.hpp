0325b82502c03ec1ef619361843bc092a84dc8be
TrueKing2002 <50130189+TrueKing2002@users.noreply.github.com>
2022-02-10 23:31:58 -0800

    reversed val and value variables

--- op.hpp
+++ op.hpp
@@ -5,12 +5,12 @@
 
 class Op : public Base {
     private:
-	double val;
+	double value;
 	std::ostringstream strs;
     public:
-        Op(double value) : Base() { val = value; }
-        virtual double evaluate() { return val; }
-        virtual std::string stringify() { strs << val; return strs.str(); }
+        Op(double val) : Base() { value = val; }
+        virtual double evaluate() { return value; }
+        virtual std::string stringify() { strs << value; return strs.str(); }
 };
 
 #endif //__OP_HPP__
